1c100298cf7d3bd12ba3c3ec30ea69ac8198e953
jerh011 <128959299+jerh011@users.noreply.github.com>
2023-09-29 11:18:54 -0700

    Add files via upload

---
+++ Metodos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1
+{
+    internal class Metodos
+    {
+        public void Primer_interativo_1(int numero_1, int numero_2)
+        {
+            Console.WriteLine("=============================================================");
+
+            Console.WriteLine("Asendente");
+            for (int X = numero_1; X <= numero_2; X++)
+                Console.WriteLine($"\"la secuencia de los números es {X}\"");
+
+
+        }
+        public void Primer_interativo_2(int numero_1, int numero_2)
+        {
+            Console.WriteLine("=============================================================");
+
+            Console.WriteLine("Desendente");
+            for (int X = numero_1; X >= numero_2; X--)
+                Console.WriteLine($"\"la secuencia de los números es {X}\"");
+        }
+
+        public int Multiplicacion(int num_1, int num_2)
+        {
+
+            if (++num_2 >= 11)
+                return 0;
+            else
+                Console.WriteLine($"{num_2}X{num_1}={num_1 * num_2}");
+
+            return Multiplicacion(num_1, num_2);
+        }
+
+     //hola
+
+    }
+}
