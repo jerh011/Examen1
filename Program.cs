06e285efdf994914f2cdbbedd0d5292d7493349c
jerh011 <128959299+jerh011@users.noreply.github.com>
2023-09-29 11:11:19 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+using Examen1;
+using System.Globalization;
+
+Metodos Ejerciciso=new Metodos();
+
+Console.WriteLine("Ingrese el numero mas chico");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Segundo el numero mas grande");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Ejerciciso.Primer_interativo_1(num1,num2);
+Ejerciciso.Primer_interativo_2(num2,num1);
+
+Console.WriteLine("Ingrese el numero a multiplicar");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Ejerciciso.Multiplicacion(num3,0);
+Console.WriteLine("");
+
